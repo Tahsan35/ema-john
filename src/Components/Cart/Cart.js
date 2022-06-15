c1ff9dd2e83d9c7eb1c10085c136a33d0a802ebd
Tahsan35 <tahsanknmas14@gmail.com>
2022-06-15 14:15:44 +0600

    update cart and local stores

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,9 +1,24 @@
-
+import './Cart.css';
 const Cart = ({cart}) => {
+    // console.log(cart)
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const pd of cart) {
+        total = total + pd.price * pd.quantity;
+        shipping = shipping + pd.shipping;
+        quantity = quantity + pd.quantity;
+    };
+    const tax = parseFloat((total* 10/100).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
-        <div>
-            <h4>Order Summary:</h4>
-            <p>Selected Item : {cart.length}</p>
+        <div className="cart">
+            <h4>Order Summary</h4>
+            <p>Selected Item : {quantity}</p>
+            <p>Total price : ${total}</p>
+            <p>Total shipping : ${shipping}</p>
+            <p>Tax : ${tax}</p>
+            <h5>Grand Total : ${grandTotal}</h5>
         </div>
     );
 };
