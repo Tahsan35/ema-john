b58bf23403ccbfa8261f75403e0c42219112fa75
Tahsan35 <tahsanknmas14@gmail.com>
2022-06-15 08:34:30 +0600

    added cart component

---
+++ src/Components/Cart/Cart.js
@@ -0,0 +1,11 @@
+
+const Cart = ({cart}) => {
+    return (
+        <div>
+            <h4>Order Summary:</h4>
+            <p>Selected Item : {cart.length}</p>
+        </div>
+    );
+};
+
+export default Cart;
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 const Shop = () => {
@@ -26,8 +27,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-            <h4>Order Summary:</h4>
-                <p>Selected Item : {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
